c9f08d1b950d130da942864c59e49c19a805b748
Nurul Amin <nurul.amin.naim@gmail.com>
2023-06-14 05:45:31 +1000

    Helmet

--- weight-loss/src/main.jsx
+++ weight-loss/src/main.jsx
@@ -4,11 +4,14 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes";
 import "./index.css";
 import AuthProvider from "./Providers/AuthProviders";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<AuthProvider>
-			<RouterProvider router={router} />
-		</AuthProvider>
+		<HelmetProvider>
+			<AuthProvider>
+				<RouterProvider router={router} />
+			</AuthProvider>
+		</HelmetProvider>
 	</React.StrictMode>
 );
