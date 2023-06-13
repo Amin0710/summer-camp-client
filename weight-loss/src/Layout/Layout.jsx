9a7e96e4701d1a50d30da071a11083d66fd55ba5
Nurul Amin <nurul.amin.naim@gmail.com>
2023-06-14 04:25:34 +1000

    Layout

--- weight-loss/src/Layout/Layout.jsx
+++ weight-loss/src/Layout/Layout.jsx
@@ -1,11 +1,22 @@
-import { Outlet } from "react-router-dom";
-import Footer from "../Pages/Shared/Footer/Footer";
+import { Outlet, useLocation } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer";
+import Navbar from "../Pages/Shared/Navbar";
 
 const Layout = () => {
+	const location = useLocation();
+
+	const noHeaderFooter = location.pathname.includes("four-oh-four");
 	return (
-		<div>
-			<Outlet></Outlet>
-			<Footer></Footer>
+		<div className="flex flex-col min-h-screen">
+			{noHeaderFooter || <Navbar></Navbar>}
+			<div className="flex-grow">
+				<Outlet></Outlet>
+			</div>
+			{noHeaderFooter || (
+				<div className="mt-auto">
+					<Footer></Footer>
+				</div>
+			)}
 		</div>
 	);
 };
