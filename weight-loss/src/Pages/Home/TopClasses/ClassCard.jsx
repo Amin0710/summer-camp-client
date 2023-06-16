24e5840ac757dd4319a96d50fa7a74e1b1d86a4d
Nurul Amin <nurul.amin.naim@gmail.com>
2023-06-17 03:53:10 +1000

    Class card done

--- weight-loss/src/Pages/Home/TopClasses/ClassCard.jsx
+++ weight-loss/src/Pages/Home/TopClasses/ClassCard.jsx
@@ -1,10 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const ClassCard = ({ classes }) => {
-	console.log(classes);
+	const location = useLocation();
+	const isHomePage = location.pathname === "/";
+
+	const adminUser = false;
+	const isSeatZero = !classes?.availableSeats;
+	const bgColor = isSeatZero && !isHomePage ? "bg-red-600" : "bg-[#00AEEF]";
 	return (
 		<div
-			className="card card-compact bg-[#00AEEF] shadow-xl p-4"
+			className={`card card-compact ${bgColor} shadow-xl p-4`}
 			style={{ width: "calc(33.33% - 8px)" }}>
 			<figure>
 				<img src={classes?.image} alt="Shoes" className="h-[350px]" />
@@ -24,10 +29,12 @@ const ClassCard = ({ classes }) => {
 						Number of students: {50 - classes?.availableSeats}
 					</div>
 				</div>
-				<div>
-					<Link to={`/games/${classes?._id}`}>
-						<button className="btn bg-[#FFFAFA] text-[#00AEEF] w-full">
-							View Details
+				<div className={`${isHomePage ? "hidden" : ""}`}>
+					<Link>
+						<button
+							className="btn bg-[#FFFAFA] text-[#00AEEF] w-full"
+							disabled={adminUser || isSeatZero}>
+							{isSeatZero ? "Full" : "Select"}
 						</button>
 					</Link>
 				</div>
